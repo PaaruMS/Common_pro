7ca997027eb96aa8dc92f70ae5b216025a0ab2ac
PaaruMS <mailtomathi77@gmail.com>
2025-09-08 14:27:01 +0530

    user

---
+++ src/test/java/com/ach/user_serv/UserServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ach.user_serv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
